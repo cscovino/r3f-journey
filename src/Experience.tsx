375fd2ce07700df980e7541905007ff31458ddd0
Carlos Scovino <kikescovino@gmail.com>
2023-03-01 18:30:26 -0300

    feat: react three fiber app

---
+++ src/Experience.tsx
@@ -0,0 +1,58 @@
+import { extend, Object3DNode, useFrame, useThree } from '@react-three/fiber';
+import { useRef } from 'react';
+import { Group, Mesh } from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import CustomObject from './CustomObject';
+
+extend({ OrbitControls });
+declare module '@react-three/fiber' {
+  interface ThreeElements {
+    orbitControls: Object3DNode<OrbitControls, typeof OrbitControls>;
+  }
+}
+
+function Experience() {
+  const { camera, gl } = useThree();
+  const groupRef = useRef<Group>(null);
+  const cubeRef = useRef<Mesh>(null);
+
+  useFrame((_, delta) => {
+    // const angle = state.clock.elapsedTime;
+    // state.camera.position.x = Math.sin(angle) * 8;
+    // state.camera.position.z = Math.cos(angle) * 8;
+    // state.camera.lookAt(0, 0, 0);
+
+    if (cubeRef.current) {
+      // && groupRef.current) {
+      cubeRef.current.rotation.y += delta;
+      // groupRef.current.rotation.y += delta;
+    }
+  });
+
+  return (
+    <>
+      <orbitControls args={[camera, gl.domElement]} />
+
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+
+      <group ref={groupRef}>
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshStandardMaterial color="orange" />
+        </mesh>
+        <mesh ref={cubeRef} rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+          <boxGeometry />
+          <meshStandardMaterial color="mediumpurple" />
+        </mesh>
+      </group>
+      <mesh rotation-x={-Math.PI * 0.5} position-y={-1} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+      <CustomObject />
+    </>
+  );
+}
+
+export default Experience;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { Canvas } from '@react-three/fiber';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ACESFilmicToneMapping, sRGBEncoding } from 'three';
+
+import Experience from './Experience';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Canvas
+      flat
+      dpr={[1, 2]}
+      gl={{ antialias: true, toneMapping: ACESFilmicToneMapping, outputEncoding: sRGBEncoding }}
+      camera={{ fov: 45, near: 0.1, far: 200, position: [3, 2, 6] }}
+    >
+      <Experience />
+    </Canvas>
   </React.StrictMode>,
-)
+);
