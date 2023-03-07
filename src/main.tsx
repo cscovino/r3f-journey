9f1e27f5531cfdde0565482ad804443fa2cf7eb0
Carlos Scovino <kikescovino@gmail.com>
2023-03-07 20:37:18 -0300

    feat: add physics

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,15 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Canvas camera={{ fov: 45, near: 0.1, far: 200, position: [-3, 1.5, 4] }}>
+    <Canvas
+      shadows
+      camera={{
+        fov: 45,
+        near: 0.1,
+        far: 200,
+        position: [4, 2, 6],
+      }}
+    >
       <Experience />
     </Canvas>
   </React.StrictMode>,
